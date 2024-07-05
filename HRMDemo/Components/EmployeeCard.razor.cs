a73be858ca72ff388abeba61f4d1dd2fda879e60
Samuel Kazan <samuel.kazan@iterates.be>
2024-07-05 19:07:51 +0200

    adding Interacitve mode (remove StreamRendering from Employee Overview)

---
+++ HRMDemo/Components/EmployeeCard.razor.cs
@@ -0,0 +1,14 @@
+﻿using DomainData;
+using Microsoft.AspNetCore.Components;
+
+namespace HRMDemo.Components
+{
+    public partial class EmployeeCard
+    {
+        [Parameter]
+        public Employee Employee { get; set; } = default!;
+
+        [Parameter]
+        public EventCallback<Employee> EmployeeQuickViewClicked { get; set; }
+    }
+}
---
+++ HRMDemo/Components/QuickViewPopup.razor.cs
@@ -0,0 +1,26 @@
+﻿using DomainData;
+using Microsoft.AspNetCore.Components;
+
+namespace HRMDemo.Components
+{
+    public partial class QuickViewPopup
+    {
+        private Employee? _employee;
+
+        [Parameter]
+        public Employee? Employee { get; set; }
+
+
+        protected override void OnParametersSet()
+        {
+            _employee = Employee;
+        }
+
+        public void Close()
+        {
+            _employee = null;
+        }
+    }
+
+    
+}
