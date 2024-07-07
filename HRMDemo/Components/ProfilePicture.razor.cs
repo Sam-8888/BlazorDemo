d2252b2b08947e7540b685a456fe6aa5d100ff10
Samuel Kazan <samuel.kazan@iterates.be>
2024-07-07 14:48:54 +0200

    Adding RenderFragment (with profile picture)

---
+++ HRMDemo/Components/ProfilePicture.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace HRMDemo.Components
+{
+    public partial class ProfilePicture
+    {
+        [Parameter]
+        public RenderFragment? ChildContent { get; set; }
+    }
+}
